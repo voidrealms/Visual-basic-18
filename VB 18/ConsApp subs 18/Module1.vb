9147b65e9eefc0d1705270fbcf1aa130807839a4
Bryan Cairns <bcairns@voidrealms.com>
2017-11-02 15:40:12 -0400

    First commit

---
+++ VB 18/ConsApp subs 18/Module1.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+    Dim icount As Integer = 0
+
+    'Sub Routines
+
+    Sub Main()
+
+        print("hello", "bob")
+        print("hi", "mary")
+        print("hola", "chad")
+
+        Console.ReadLine()
+
+    End Sub
+
+    Sub Counter()
+        icount += 1
+        Console.WriteLine()
+
+    End Sub
+
+    Sub print(ByVal message As String, ByVal name As String)
+        Console.WriteLine("from " & name & " message =" & message)
+    End Sub
+
+End Module
---
+++ readme.txt
@@ -0,0 +1,10 @@
+Code for video tutorial
+
+Title: Visual basic 18 - Sub Routines
+Published: 7/11/2010 12:05:57 PM
+Link: https://www.youtube.com/watch?v=BHxb4F657LY
+
+Description: Learn about sub routines
+
+If you found this helpful, please consider donating: http://www.voidrealms.com
+
